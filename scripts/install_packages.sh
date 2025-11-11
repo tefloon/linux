d57be9bcbcccf14c4affc6586c73202c4a79c57c
Antoni Gawlikowski <tefloon@gmail.com>
2025-11-11 15:25:17 +0100

    Added color picker

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -151,3 +151,4 @@ install_aur_pkg "sublime-text-4"          # Text editor
 install_aur_pkg "helium-browser-bin"      # Privacy-focused web browser (my main)
 install_aur_pkg "adw-gtk-theme-git"       # Adwaita GTK theme (development version)
 install_aur_pkg "freeoffice"              # Office suite
+install_aur_pkg "hyprpicker"              # Wayland color picker utility
