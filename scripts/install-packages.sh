1321a24710030eb3631f09618ab315d1b2a47a97
Antoni Gawlikowski <tefloon@gmail.com>
2026-08-24 12:21:52 +0200

    Added aria2 to installed packages

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -97,6 +97,7 @@ install_pkg "sd"                          # Modern replacement for sed
 install_pkg "ugrep"                       # Search files for strings
 install_pkg "translate-shell"             # Terminal translator
 install_pkg "urban-cli-bin"               # Urban dictionary CLI
+install_pkg "aria2"                       # CLI Torrent downloader
 
 # --- PYTHON PACKAGES ---
 install_pkg "python-astral"               # Needed for the moon phase script
